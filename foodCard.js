a40c8bc8fcbbf4cc1e19edc9acf7bf8e6d13baba
mushfiqur2002 <mushfiqurnasim@gmail.com>
2024-10-06 02:54:06 +0600

    add menu

--- foodCard.js
+++ foodCard.js
@@ -8,31 +8,39 @@ let currentIndex = 0;
 let cardShow = 4;
 let screenSize = window.innerWidth;
 
-if(screenSize<950){
+if (screenSize < 950) {
     cardShow = 3;
 }
-if(screenSize<900){
+if (screenSize < 900) {
     cardShow = 2;
 }
 
 async function loadData() {
     const data = await fetchData(); // Await the fetchData call 
+
+    console.log(data,currentPage)
     // Check if current page is 'index.html' or ""
     if (currentPage === 'index.html' || currentPage === "") {
         // Filter data to only those with a star rating above 4.5
         filterItems = data.filter(item => item.star > 4.5);
+    }else if (currentPage === 'menu.html') {
+        filterItems = data; // Show all items in menu.html
     }
     displayCard();
 }
 
 function displayCard() {
-    let selectItem =[];
+    let selectItem = [];
     let star = currentIndex * cardShow;
     let end = star + cardShow;
-    if (currentPage === 'index.html' || currentPage === ""){
-        selectItem = filterItems.slice(star,end);
+    if (currentPage === 'index.html' || currentPage === "") {
+        selectItem = filterItems.slice(star, end);
+        prevBtn.disabled = currentIndex === 0;
+        nextBtn.disabled = end > filterItems.length;
+    }else if (currentPage === 'menu.html') {
+        selectItem = filterItems;
     }
-    let element = selectItem.map(function(item) {
+    let element = selectItem.map(function (item) {
         return `<div class="card">
                     <div class="addCart">
                         <button class="btn"><i class="fa-solid fa-cart-plus"></i></button>
@@ -55,22 +63,22 @@ function displayCard() {
 
     cardContainer.innerHTML = element;
 
-    prevBtn.disabled = currentIndex === 0;
-    nextBtn.disabled = end> filterItems.length;
 }
 
-prevBtn.addEventListener("click",function(){
-    if (currentIndex > 0) {
-        currentIndex--;
-        displayCard();
-    }
-})
-nextBtn.addEventListener("click",function(){
-    if ((currentIndex + 1) * cardShow < filterItems.length) {
-        currentIndex++;
-        displayCard();
-    }
-})
+if (currentPage === 'index.html' || currentPage === "") {
+    prevBtn.addEventListener("click", function () {
+        if (currentIndex > 0) {
+            currentIndex--;
+            displayCard();
+        }
+    })
+    nextBtn.addEventListener("click", function () {
+        if ((currentIndex + 1) * cardShow < filterItems.length) {
+            currentIndex++;
+            displayCard();
+        }
+    })
+}
 
 loadData();
 
